ad442fc6cce5287dfc3b9178f8c3f9f2354d3ec1
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-03-31 20:47:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,4 @@ with col1:
 
 with col2:
     st.title('Quem sou eu?')
-    st.divider()
     st.write('Meu nome é Hanna Patrocinio, sou desenvolvedora em python, artista em pixel, aspirante à poeta e musicista. Comecei na área de programação há pouco tempo, tenho formação em Python e MySQL. Sou artista desde que me conheço por gente, arte é vida. Eu aprendo rapido, sou proativa e tenho uma boa comunicação. Gosto de inovar e evoluir cada vez mais, quebrando minnhas barreiras pessoais e aprendendo mais e mais coisas. Conheça mais sobre mim neste site!')
