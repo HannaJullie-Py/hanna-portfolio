436cc2774285ea6fc171bf7545bbf1ae167692ba
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-03-31 20:59:26 -0300

    Update 0_Desenvolvimento_Python.py

--- pages/0_Desenvolvimento_Python.py
+++ pages/0_Desenvolvimento_Python.py
@@ -1,1 +1,4 @@
-
+st.set_page_config(
+    page_title='Desenvolmento Python',
+    page_icon='https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png', # Esse é um URL como Favicon, pode ser um emoji ou outra imagem.
+)
