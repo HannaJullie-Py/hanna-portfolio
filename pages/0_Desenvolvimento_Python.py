993c61d41c3fba0ba63ed3c457b92f971433715e
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-03-31 20:59:59 -0300

    Update 0_Desenvolvimento_Python.py

--- pages/0_Desenvolvimento_Python.py
+++ pages/0_Desenvolvimento_Python.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.set_page_config(
     page_title='Desenvolmento Python',
     page_icon='https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png', # Esse é um URL como Favicon, pode ser um emoji ou outra imagem.
