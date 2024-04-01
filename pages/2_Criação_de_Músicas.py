0b6159ef92d7c656affd8f7a995554992e90d266
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-04-01 20:34:24 -0300

    Update 2_Criação_de_Músicas.py

--- pages/2_Criação_de_Músicas.py
+++ pages/2_Criação_de_Músicas.py
@@ -11,8 +11,6 @@ st.set_page_config(
 st.title('Criação de Músicas')
 st.write('Nesta Página, você verá as melhores músicas que eu fiz nesses longos anos como produtora musical. Aproveite!')
 st.divider()
-col1, col2, col3 = st.columns(3)
-with col2:
-    st.write('Hanna Jullie - Sangue Azul')
+st.title('Hanna Jullie - Sangue Azul')
 st.video("https://www.youtube.com/watch?v=wuXSKruNcaA")
 st.divider()
