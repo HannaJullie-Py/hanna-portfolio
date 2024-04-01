4f94a1b539a81328b4f71a955e79a0052f58b9bc
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-03-31 21:06:17 -0300

    Update 0_Desenvolvimento_Python.py

--- pages/0_Desenvolvimento_Python.py
+++ pages/0_Desenvolvimento_Python.py
@@ -6,3 +6,9 @@ st.set_page_config(
     page_title='Desenvolmento Python',
     page_icon='https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png', # Esse é um URL como Favicon, pode ser um emoji ou outra imagem.
 )
+
+col1, col2, col3 = st.columns(3)
+
+with col2:
+    st.title("Desenvolvimento Python")
+    st.write("Nesta pagina você irá ver alguns dos meus dotes no python. Projetos e joguinhos não estão de fora. Se divirte e veja se gosta de como eu escrevo meus códigos!!!")
