8b7b4d93dd3df731cbabf9727a0652148c1ab008
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-03-31 21:39:40 -0300

    Update 0_Desenvolvimento_Python.py

--- pages/0_Desenvolvimento_Python.py
+++ pages/0_Desenvolvimento_Python.py
@@ -14,7 +14,3 @@ st.set_page_config(
 st.header("Desenvolvimento Python")
 st.write("Nesta pagina você irá ver alguns dos meus dotes no python. Projetos e joguinhos não estão de fora. Se divirta e veja se gosta de como eu escrevo meus códigos!!!")
 st.divider
-st.title("Tap Code Translate")
-st.write("Código que traduz português para uma codifição chamada Tap Code. obs: não use acentos!!!")
-code = subprocess.run(['python','https://github.com/HannaJullie-Py/hanna-portfolio/blob/main/pages/tap_code_translate_by_hanna_(ver_0_1).py'])
-st.code(code)
