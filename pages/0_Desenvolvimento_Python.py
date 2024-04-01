ee04832b0b1651f1fe2f1444d9cb0e810d3d9ca4
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-04-01 18:59:37 -0300

    Update 0_Desenvolvimento_Python.py

--- pages/0_Desenvolvimento_Python.py
+++ pages/0_Desenvolvimento_Python.py
@@ -192,91 +192,91 @@ if opcao == 'Chuveiro Elétrico':
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Ar Condicionado':
     pot = 1600
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Ferro de Passar':
     pot = 1500
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Computador':
     pot = 550
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Geladeira':
     pot = 100
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'TV':
     pot = 150
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Ventilador':
     pot = 100
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Máquina de Lavar Roupa':
     pot = 1500
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Liquidificador':
     pot = 400
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Aspirador de Pó':
     pot = 1500
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Micro-ondas':
     pot = 1300
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Notebook':
     pot = 50
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Impressora':
     pot = 50
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Impressora 3D':
     pot = 50
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.')
 if opcao == 'Portão Elétrico':
     pot = 745.7
     con = pot*hd*nd/1000
     with st.spinner('Carregando...'):
         time.sleep(5)
-    st.write(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.', )
+    st.caption(f'Seu gasto mensal será de {con:.2f} Kw/h, portanto R$ {0.48*con:.2f}.', )
 
 st.divider()
 col8, col9, col0 = st.columns(3)
