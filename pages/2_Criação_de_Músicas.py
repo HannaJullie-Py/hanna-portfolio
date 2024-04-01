8d790f7da60acff314fbffc6d689a45af7f841a0
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-04-01 20:33:47 -0300

    Update 2_Criação_de_Músicas.py

--- pages/2_Criação_de_Músicas.py
+++ pages/2_Criação_de_Músicas.py
@@ -13,6 +13,6 @@ st.write('Nesta Página, você verá as melhores músicas que eu fiz nesses long
 st.divider()
 col1, col2, col3 = st.columns(3)
 with col2:
-    st.markdown('##Hanna Jullie - Sangue Azul')
+    st.write('Hanna Jullie - Sangue Azul')
 st.video("https://www.youtube.com/watch?v=wuXSKruNcaA")
 st.divider
