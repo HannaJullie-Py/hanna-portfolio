9960767260b66fb349f70d43dcf184d2d15372bf
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-03-31 21:54:54 -0300

    Update 1_Pixel_ Arts.py

--- pages/1_Pixel_ Arts.py
+++ pages/1_Pixel_ Arts.py
@@ -1,3 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.set_page_config(
+    page_title='Pixel Arts',
+    page_icon='🖌️', # Esse é um emoji como Favicon, pode ser um URL ou outra imagem.
+)
+
+st.title("Pixel Arts")
+st.write("Nesta página você vê algumas das muitas pixel arts que fiz durante todos os meus anos. Aproveite!")
+st.divider()
+st.title('Cenários')
+st.image("https://i.imgur.com/Rg4nTKk.jpeg")
+st.image("https://i.imgur.com/puiqQTj.jpeg")
+st.title('Ideias Abstratas')
