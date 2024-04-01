500db636aa421d33e48a69061918b7724e37bcb1
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-04-01 20:30:08 -0300

    Update 2_Criação_de_Músicas.py

--- pages/2_Criação_de_Músicas.py
+++ pages/2_Criação_de_Músicas.py
@@ -4,7 +4,12 @@ import numpy as np
 import time 
 
 st.set_page_config(
-    page_title='Minhas Músicas',
+    page_title='Criação de Músicas',
     page_icon='https://github.com/HannaJullie-Py/hanna-portfolio/blob/main/download.png?raw=true'
 )
 
+st.title('Criação de Músicas')
+st.write('Nesta Página, você verá as melhores músicas que eu fiz nesses longos anos como produtora musical. Aproveite!')
+st.divider()
+st.video("https://www.youtube.com/watch?v=wuXSKruNcaA")
+st.caption('Hanna Jullie - Sangue Azul')
